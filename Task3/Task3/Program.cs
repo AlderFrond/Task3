514a9ca4c45bc26c185c89985db6dd60884265af
AlderFrond <AlderFrond@yandex.ru>
2014-10-15 20:20:49 +0400

    Task3

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -10,6 +10,90 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            Console.Write("Ввести коллич. элем. массива ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];//индекс с нуля
+
+            //while(условия работы цикла) { ... }
+            //цикл с предусловием. 0..N
+
+
+            //Делает, потом думает. 1 .. N
+            //do{ ... } while(условия работы программы)
+
+
+            //for (Переменная внутри цикла; Уловия работы; То что делается в конце цикла\ приращение;) { ... }
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)//здесь будет max
+            {
+
+            }
+            Console.Write("{0} ", a[i]);
+            for (int i = 0; i < n; i++)//здесь будет min
+            {
+
+            }
+            Console.Write("{0} ", a[i]);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+                
+
+            Console.ReadKey();
+
+            /*
+             //int i = 0;
+           // while (i < n)//Ввод массива
+           // {
+           //     a[i] = int.Parse(Console.ReadLine());
+           //     i++;
+           // }
+             //i = 0;
+                //while (i < n)//Вывод
+                // {
+                //     Console.Write("{0} ", a[i]);
+                //     i++;
+                // }
+            a[0] = int.Parse(Console.ReadLine());
+            a[1] = int.Parse(Console.ReadLine());
+            a[2] = int.Parse(Console.ReadLine());
+            a[3] = int.Parse(Console.ReadLine());
+            a[4] = int.Parse(Console.ReadLine());
+            a[5] = int.Parse(Console.ReadLine());
+            a[6] = int.Parse(Console.ReadLine());
+            a[7] = int.Parse(Console.ReadLine());
+            a[8] = int.Parse(Console.ReadLine());
+            a[9] = int.Parse(Console.ReadLine());
+            a[10] = int.Parse(Console.ReadLine());
+            a[11] = int.Parse(Console.ReadLine());
+            a[12] = int.Parse(Console.ReadLine());
+            a[13] = int.Parse(Console.ReadLine());
+            a[14] = int.Parse(Console.ReadLine());
+
+            Console.Write("{0} ", a[0]);
+            Console.Write("{0} ", a[1]);
+            Console.Write("{0} ", a[2]);
+            Console.Write("{0} ", a[3]);
+            Console.Write("{0} ", a[4]);
+            Console.Write("{0} ", a[5]);
+            Console.Write("{0} ", a[6]);
+            Console.Write("{0} ", a[7]);
+            Console.Write("{0} ", a[8]);
+            Console.Write("{0} ", a[9]);
+            Console.Write("{0} ", a[10]);
+            Console.Write("{0} ", a[11]);
+            Console.Write("{0} ", a[12]);
+            Console.Write("{0} ", a[13]);
+            Console.Write("{0} ", a[14]);
+
+             */
+
         }
     }
 }
